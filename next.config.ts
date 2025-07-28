1d5f9922305dae4c7bbfee6f62127ff098870774
Yan Lima Barbosa <yanbr763@gmail.com>
2025-07-28 06:24:40 -0300

    feat: add Dockerfile and update Next.js configuration
    - Introduced a Dockerfile for containerized development with Node.js.
    - Updated Next.js configuration to include custom webpack watch options for improved build performance.
    - Added a new script in package.json for development with watch mode.
    - Enhanced favicon SVG for better visual consistency.
    - Made minor adjustments to global styles and various component sections for improved UI consistency.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
